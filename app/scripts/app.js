5ac3e7c82555a862de1aa07c17683aac3c3b4c96
Fredrik Wollsén <fredrik@neam.se>
2013-05-16 17:02:40 +0200

    Implementation and demo of messageHandler.sendMessage()

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,31 @@
 define(['jquery'], function($) {
 	'use strict';
 
+	var messageHandler = {
+		sendMessage: function(data, callback) { // analogue of chrome.runtime.sendMessage
+			var request = document.createTextNode(JSON.stringify(data));
+
+			request.addEventListener("messageHandler-response", function(event) {
+				request.parentNode.removeChild(request);
+				if (callback) {
+					var response = JSON.parse(request.nodeValue);
+					callback(response);
+				}
+			}, false);
+
+			document.head.appendChild(request);
+
+			var event = document.createEvent("HTMLEvents");
+
+			event.initEvent("messageHandler-query", true, false);
+			request.dispatchEvent(event);
+
+		},
+		sampleCallback: function(response) {
+			return alert("response: " + (response ? response.toSource() : response));
+		}
+	};
+
 	function triggerEvent(name) {
 
 		var element = document.createElement("foo-data-element");
@@ -19,6 +44,30 @@ define(['jquery'], function($) {
 		console.log('@testNotification');
 		e.preventDefault();
 		triggerEvent("testNotification");
+	});
+
+	$('.btn.action-testSendMessage1').click(function(e) {
+		console.log('@testSendMessage1');
+		e.preventDefault();
+		messageHandler.sendMessage({foo: 1}, messageHandler.sampleCallback);
+	});
+
+	$('.btn.action-testSendMessage2').click(function(e) {
+		console.log('@testSendMessage2');
+		e.preventDefault();
+		messageHandler.sendMessage({baz: 3}, messageHandler.sampleCallback);
+	});
+
+	$('.btn.action-testSendMessage3').click(function(e) {
+		console.log('@testSendMessage3');
+		e.preventDefault();
+		messageHandler.sendMessage({mozilla: 3}, messageHandler.sampleCallback);
+	});
+
+	$('.btn.action-testSendMessage4').click(function(e) {
+		console.log('@testSendMessage4');
+		e.preventDefault();
+		messageHandler.sendMessage({firefox: 4}, messageHandler.sampleCallback);
 	});
 
 	$('.btn.action-openAboutDialog').click(function(e) {
--- bridge/main.js
+++ bridge/main.js
@@ -10,6 +10,8 @@ var Bridge = {
 			if (e.target.location.hostname === 'localhost') {
 				var doc = e.target;
 
+				// Some simple listeners
+
 				doc.addEventListener("testNotification", function(e) {
 					App.debug('testNotification');
 					App.nativeNotification('Native alert', 'Looks like this');
@@ -29,6 +31,49 @@ var Bridge = {
 					App.debug('checkForUpdates');
 					App.MainUI.checkForUpdates();
 				}, false);
+
+				// A more advanced message handler derived from the sample code on https://developer.mozilla.org/en-US/docs/Code_snippets/Interaction_between_privileged_and_non-privileged_pages
+
+				var messageHandler = {
+					addListener: function(callback) { // analogue of chrome.runtime.onMessage.addListener
+						var listener = doc.addEventListener("messageHandler-query", function(event) {
+							var node = event.target;
+							if (!node || node.nodeType !== 3) // Node.TEXT_NODE = 3
+								return;
+
+							var _doc = node.ownerDocument;
+							callback(JSON.parse(node.nodeValue), _doc, function(response) {
+								node.nodeValue = JSON.stringify(response);
+
+								var event = _doc.createEvent("HTMLEvents");
+								event.initEvent("messageHandler-response", true, false);
+								return node.dispatchEvent(event);
+							});
+						}, false, true);
+						return listener;
+					},
+					receiveMessage: function(request, sender, sendResponse) {
+
+						if (request.foo) {
+							sendResponse({bar: 2});
+						}
+
+						if (request.baz) {
+							window.setTimeout(function() {
+								sendResponse({quux: 4, msg: 'Had you waiting for 3 seconds...'});
+							}, 3000);
+							return null;
+						}
+
+						if (request.mozilla) {
+							return App.nativeNotification("Mozilla?", "Replying with notification...");
+						}
+
+						return sendResponse(null);
+					}
+				};
+
+				messageHandler.addListener(messageHandler.receiveMessage);
 
 			}
 		}, false);
